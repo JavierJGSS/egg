de87a450d5c54212db10923d5411c56b0c3a9008
“Javier <garciajavierj1986@gmail.com>
2023-03-22 21:48:55 -0300

    Nuevo commit

---
+++ src/nuevoproyecto/HolaMundo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevoproyecto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Javier
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        Scanner leer = new Scanner (System.in);
+          String nombre;      
+          System.out.println("Ingrese tu nombre");
+          nombre = leer.next();
+          int numero;
+          numero = 20;
+          
+          System.out.println("Hola Mundo! Soy " + nombre + " y estoyyyyy programando en Java");
+    }
+    
+}
---
+++ src/nuevoproyecto/JavaApplication3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevoproyecto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Javier
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int num1, num2,num3;
+        System.out.println("Ingrese dos numeros");
+        num1 = leer.nextInt();
+        num2 = leer.nextInt();
+        num3 = num1 + num2;
+        System.out.println("La suma de los dos numeros es: " + num3);
+        
+    }
+
+ }
+    
+    
+
